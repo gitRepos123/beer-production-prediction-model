1a8f7dd68f0e93fd954183424d769c88eeb61e69
Samarth Chaurasia <104571426+gitRepos123@users.noreply.github.com>
2024-04-02 05:28:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,10 @@ def make_response(days: int = 1) -> None:
     days_array = [ day+1 for day in range(days) ]
     model = joblib.load('beer-production-prediction-model.pk1')
     predictions = model.forecast(steps = days)
+    df = pd.DataFrame({
+        'Day': days_array,
+        'Production': predictions
+    })
     st.subheader('Beer Production Trend')
     fig = plt.figure()
     plt.xlabel('Day')
@@ -15,10 +19,6 @@ def make_response(days: int = 1) -> None:
     plt.plot(df['Day'], df['Production'], color = 'green') 
     st.pyplot(fig)
     st.subheader('Predictions')
-    df = pd.DataFrame({
-        'Day': days_array,
-        'Production': predictions
-    })
     st.table(df)
 
 def init_app():
