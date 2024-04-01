2bedd29b85b0cad64d8edac3e7a5adae456ec91e
Samarth Chaurasia <104571426+gitRepos123@users.noreply.github.com>
2024-04-02 05:27:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,24 @@ import joblib
 
 st.header('Beer Production Prediction - ARIMA Model')
 def make_response(days: int = 1) -> None:
-    st.subheader('Predictions')
     days_array = [ day+1 for day in range(days) ]
     model = joblib.load('beer-production-prediction-model.pk1')
     predictions = model.forecast(steps = days)
-    df = pd.DataFrame({
-        'Day': days_array,
-        'Production': predictions
-    })
-    st.table(df)
     st.subheader('Beer Production Trend')
     fig = plt.figure()
     plt.xlabel('Day')
     plt.ylabel('Production')
     plt.plot(df['Day'], df['Production'], color = 'green') 
     st.pyplot(fig)
+    st.subheader('Predictions')
+    df = pd.DataFrame({
+        'Day': days_array,
+        'Production': predictions
+    })
+    st.table(df)
 
 def init_app():
-    days = st.slider('Number of Days', min_value = 1, max_value = 135, step = 1)
+    days = st.slider('Number of Days', min_value = 1, max_value = 500, step = 1)
     make_response(days)
 
 init_app()
