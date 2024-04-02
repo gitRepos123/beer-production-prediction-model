447e9bee03a23d46759b02fe65f8fc1ae9950950
Samarth Chaurasia <104571426+gitRepos123@users.noreply.github.com>
2024-04-02 05:31:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,25 +4,25 @@ import matplotlib.pyplot as plt
 import joblib
 
 st.header('Beer Production Prediction - ARIMA Model')
-def make_response(days: int = 1) -> None:
-    days_array = [ day+1 for day in range(days) ]
+def make_response(months: int = 1) -> None:
+    months_array = [ month+1 for month in range(months) ]
     model = joblib.load('beer-production-prediction-model.pk1')
-    predictions = model.forecast(steps = days)
+    predictions = model.forecast(steps = months)
     df = pd.DataFrame({
-        'Day': days_array,
+        'Month': months_array,
         'Production': predictions
     })
     st.subheader('Beer Production Trend')
     fig = plt.figure()
-    plt.xlabel('Day')
+    plt.xlabel('Month')
     plt.ylabel('Production')
-    plt.plot(df['Day'], df['Production'], color = 'green') 
+    plt.plot(df['Month'], df['Production'], color = 'green') 
     st.pyplot(fig)
     st.subheader('Predictions')
     st.table(df)
 
 def init_app():
-    days = st.slider('Number of Days', min_value = 1, max_value = 20, step = 1)
+    days = st.slider('Number of Months', min_value = 1, max_value = 6, step = 1)
     make_response(days)
 
 init_app()
